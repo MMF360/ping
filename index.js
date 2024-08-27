c5e07f8872e28d07b4cb2a137bb931eace42a192
MM360 <mm360@MM360s-MacBook-Pro.local>
2024-08-27 08:54:40 -0600

    ping

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
+/* Serve static files from the React app */
 app.use(express.static(path.join(__dirname, '../ping/build')));
 
+/* Allows app to handle JSON POST data */
+app.use(express.json());
+
+/* All access from all origins */
+app.use(cors());
+
+/* No cache */
+app.use(nocache());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../ping/build', 'index.html'));
