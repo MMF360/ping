d8c810b86c7d6ab35b1a06cca5526cd9699a43ed
MM360 <mm360@MM360s-MacBook-Pro.local>
2024-08-27 09:12:34 -0600

    ping

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import PingWidget from './components/ping.tsx';
-import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
